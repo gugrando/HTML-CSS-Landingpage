609528dbde8f961c67f0b6a95185cbcdbdd1c61a
Gustavo Grando Vieira <97984848+gugrando@users.noreply.github.com>
2023-01-18 08:02:37 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let overlay = document.querySelector(".overlay")
+const body = document.querySelector("body")
+
+window.addEventListener("load", function(){
+    overlay.style.display = "none";
+    body.style.overflow = "auto"
+})
